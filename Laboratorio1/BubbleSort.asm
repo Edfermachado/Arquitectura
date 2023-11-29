1ae8bba3dc45706124090c6a08f6a120ee88fe53
Edwin Machado <edfermachado10@gmail.com>
2023-11-28 23:20:39 -0400

    Agregando modificacion a BubbleSort

--- Laboratorio1/BubbleSort.asm
+++ Laboratorio1/BubbleSort.asm
@@ -136,24 +136,24 @@ BubbleSort:
 	add $s0, $zero, $zero	# El registro $s0 será i
 	addi $s2, $s2, -4	# Se le resta un entero al tamanno en $s2 para no salirse del tamanno
 for1:
-	beq $s0, $s2, salir2	# Si i = tam - 1 se sale del ciclo principal
+	beq $s0, $s2, f_for2	# Si i = tam - 1 se sale del ciclo principal
 	add $s1, $zero, $zero	# $s1 se inicializa en 0
 for2:
-	beq $s1, $s2, salir1	# Si j = tam se sale del ciclo secundario
+	beq $s1, $s2, f_for1	# Si j = tam se sale del ciclo secundario
 	add $t0, $a1, $s1	# la direccion principal del vector + j dan la direccion v[j]
 	lw $t1, 0($t0)		# $t1 = v[j]
 	lw $t2, 4($t0)		# $t2 = v[j+1]
 	slt $t3, $t2, $t1	# $t3 = 1 si v[j+1] < j[j] sino $t3 = 0
-	beq $t3, $zero, noCambio # Si $t3 = 0 evita intercambiar v[j] y v[j+1]
+	beq $t3, $zero, no_swap # Si $t3 = 0 evita intercambiar v[j] y v[j+1]
 	sw $t2, 0($t0)		# Esta linea y la de abajo intercambian v[j] y v[j+1]
 	sw $t1, 4($t0)
-noCambio:s
+no_swap:
 	addi $s1, $s1, 4	# j = j + 1
 	j for2		
-salir1:
+f_for1:
 	addi $s0, $s0, 4	# i = i + 1
 	j for1		
-salir2:
+f_for2:
 	lw $s2, 0($sp)		# Restaurar el valor original de $s2
 	lw $s1, 4($sp)		# Restaurar el valor original de $s1
 	lw $s0, 8($sp)		# Restaurar el valor original de $s0
